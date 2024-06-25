2f6ae3ec68522d977d81bda5b11b55052424447a
hunglonghung <emyeuanh18042004@gmail.com>
2024-06-25 19:48:07 +0700

    Update GameManager.cs

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -32,7 +32,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        UpdateGameState(GameState.Home);
+        UpdateGameState(GameState.Loading);
     }
 
     // Update is called once per frame
